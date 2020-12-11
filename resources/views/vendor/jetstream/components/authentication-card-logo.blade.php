cd12ef824a92252a1b396a12e27993d601b8af77
JesusMarino0817 <70980307+JesusMarino0817@users.noreply.github.com>
2020-12-10 21:28:00 -0600

    Se personalizó la vista del login

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('img/myLogo2.png')}}" alt="Logo Electronica" class="img-fluid mx-auto d-block">
+    <img src="{{ asset('img/myLogo2.png')}}" alt="Logo Electronica" class="img-fluid mx-auto d-block" width="200" height="200" >
 </a>
